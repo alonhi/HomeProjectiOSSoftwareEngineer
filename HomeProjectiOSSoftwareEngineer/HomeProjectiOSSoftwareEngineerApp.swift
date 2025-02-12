a8fe5aa732b1b4d485561801899ae868452fc78b
idttm <andrew79911997@gmail.com>
2025-02-12 16:32:52 -0600

    Initial Commit

---
+++ HomeProjectiOSSoftwareEngineer/HomeProjectiOSSoftwareEngineerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeProjectiOSSoftwareEngineerApp.swift
+//  HomeProjectiOSSoftwareEngineer
+//
+//  Created by Andrew Cheberyako on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeProjectiOSSoftwareEngineerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
